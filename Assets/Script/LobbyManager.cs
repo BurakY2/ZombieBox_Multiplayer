0d98fe5a37cb75771b1a5e82ffb64bc43eefa769
Burak Yalçın <yalcnburakk@gmail.com>
2022-01-11 12:23:18 +0300

    Add new uı

--- Assets/Script/LobbyManager.cs
+++ Assets/Script/LobbyManager.cs
@@ -9,78 +9,23 @@ using System;
 
 public class LobbyManager : GlobalEventListener
 {
-
-    public void StartServer()
-    {
-        BoltLauncher.StartServer();
-    }
-
-    public override void BoltStartDone()
-    {
-        BoltMatchmaking.CreateSession("Room1", sceneToLoad:"GameScene");
-    }
-
-    public void StartClient()
-    {
-        BoltLauncher.StartClient();
-    }
-
-    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
-    {
-       foreach(var session in sessionList)
-        {
-            UdpSession photonSession = session.Value as UdpSession;
-            if(photonSession.Source == UdpSessionSource.Photon)
-            {
-                BoltMatchmaking.JoinSession(photonSession);
-            }
-        }
-    }
-
-
-    /*
     public Button joinGameButtonPrefab;
     public GameObject ServerListPanel;
     public GameObject setUsernamePanel;
     public float buttonSpacing;
     private List<Button> joinServerButtons = new List<Button>();
 
-    
-
-    public void Start()
-    {
-        Debug.Log("Asıl " + PlayerPrefs.GetString("username"));
-        setUsernamePanel.SetActive(true);
-        
-    }
-    
-
-    public void OnSetUsernameValueChange(string input)
-    {
-        print(input);
-        PlayerPrefs.SetString("username", input);
-        Debug.Log("Asıl " + PlayerPrefs.GetString("username"));
-    }
-
     public void StartServer()
     {
-        
         BoltLauncher.StartServer();
     }
 
     public override void BoltStartDone()
     {
-        if (BoltNetwork.IsServer)
-        {
-            int randomInt = UnityEngine.Random.Range(0, 9999);
-            string matchName = "Room1"+randomInt;
-            
-            //BoltNetwork.SetServerInfo(matchName, null);
-           // BoltNetwork.LoadScene("GameScene");
-            BoltMatchmaking.CreateSession(sessionID: matchName, sceneToLoad: "GameScene");
-            
-        }
-        
+        //BoltMatchmaking.CreateSession("Room1", sceneToLoad:"GameScene");
+        int randomInt = UnityEngine.Random.Range(0, 9999);
+        string matchName = "Room1" + randomInt;
+        BoltMatchmaking.CreateSession(sessionID: matchName, sceneToLoad: "GameScene");
     }
 
     public void StartClient()
@@ -94,44 +39,51 @@ public class LobbyManager : GlobalEventListener
 
         int totalsessions = 0;
         print("totalSessions initial:" + totalsessions);
-        foreach(var session in sessionList)
+
+
+        foreach (var session in sessionList)
         {
             UdpSession photonSession = session.Value as UdpSession;
 
             Button joinGameButtonClone = Instantiate(joinGameButtonPrefab);
             joinGameButtonClone.transform.parent = ServerListPanel.transform;
-            joinGameButtonClone.transform.localPosition = new Vector3(0, buttonSpacing* joinServerButtons.Count,0);
+            joinGameButtonClone.transform.localPosition = new Vector3(0, buttonSpacing * joinServerButtons.Count, 0);
             joinGameButtonClone.gameObject.SetActive(true);
             joinGameButtonClone.onClick.AddListener(() => JoinGame(photonSession));
 
             joinServerButtons.Add(joinGameButtonClone);
-            
-            /*
+
+        }
+
+
+        /*
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
             if(photonSession.Source == UdpSessionSource.Photon)
             {
                 BoltMatchmaking.JoinSession(photonSession);
             }
-            //yıldız/
-
         }
+        */
     }
-        
 
-   
+
+
     private void JoinGame(UdpSession photonSession)
     {
         BoltMatchmaking.JoinSession(photonSession);
     }
 
+
     private void ClearSessions()
     {
         Debug.Log("Clear sessions call");
-        foreach(Button button in joinServerButtons)
+        foreach (Button button in joinServerButtons)
         {
             Destroy(button.gameObject);
         }
 
         joinServerButtons.Clear();
     }
-    */
 }
